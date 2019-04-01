6129fa1a13b9097e2b778d7db34722262db05a13
RaymundPamati-an <46916731+RaymundPamati-an@users.noreply.github.com>
2019-04-01 08:51:28 +0800

    Added more things

--- btokens.py
+++ btokens.py
@@ -21,8 +21,8 @@ t_MINUS            = r'-'
 t_TIMES            = r'\*'
 t_DIVIDE           = r'/'
 t_MODULO           = r'%'
-t_OR               = r'\|'
-t_AND              = r'&'
+t_OR               = r'\|\|'
+t_AND              = r'\&\&'
 t_NOT              = r'~'
 t_LT               = r'<'
 t_GT               = r'>'
@@ -56,3 +56,67 @@ t_FLTLIT = r'((\d+)(\.\d+)(e(\+|-)?(\d+))? | (\d+)e(\+|-)?(\d+))([lL]|[fF])?'
 
 # String literal
 t_STRLIT = r'\"([^\\\n]|(\\.))*?\"'
+
+def t_CPPCOMMENT(t):
+    r'//.*\n'
+    t.lexer.lineno += 1
+    return t
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+t_ignore = " \t"
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+#
+import ply.lex as lex
+lex.lex()
+
+#parsing rules
+precedence = (
+		('right', '='),
+		('left', '||'),
+		('left', '&&'),
+		('left', '==', '!='),
+		('left', '<', '<=', '>', '>='),
+		('left', '+', '-'),
+		('left', '*', '/', '%'),
+		('left', '[]', '()'),
+
+	)
+
+#dictionary of names
+names = {}
+
+def p_statement_expr(p):
+    'statement : expression'
+    print(p[1])
+
+def p_expression_binop(p):
+    '''expression : expression '+' expression
+                  | expression '-' expression
+                  | expression '*' expression
+                  | expression '/' expression
+                  | expression '%' expression'''
+    if p[2] == '+':
+        p[0] = p[1] + p[3]
+    elif p[2] == '-':
+        p[0] = p[1] - p[3]
+    elif p[2] == '*':
+        p[0] = p[1] * p[3]
+    elif p[2] == '/':
+        p[0] = p[1] / p[3]
+    elif p[2] == '%':
+    	p[0] = p[1] % p[3]
+
+import ply.yacc as yacc
+yacc.yacc()
